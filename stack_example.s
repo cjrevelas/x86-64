0e2fcb5a1a9f6fd2bf6654cfac03a247585aa038
cjrevelas <cjrevelas@gmail.com>
2026-02-22 17:18:48 +0200

    use macros include file

--- stack_example.s
+++ stack_example.s
@@ -1,18 +1,5 @@
-; The following macro sets up the the stack frame of a function.
-;
-%macro prologue 0
-    push rbp                   ; save rbp on the stack to restore it at the end of the function
-    mov rbp, rsp               ; set rbp to rsp
-%endmacro
 
-;
-; The following macro restores rbp from the stack and returns to the caller.
-;
-%macro epilogue 0
-    mov rsp, rbp               ; discard local variables and restore stack pointer
-    pop rbp                    ; restore the old frame pointer
-    ret
-%endmacro
+%include "macros.s"
 
 %define SYSCALL_EXIT 60
 
